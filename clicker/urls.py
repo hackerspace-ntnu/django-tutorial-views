85e941af91d3e60b4242622222ee0e775e9ebdb5
Amund Tenstad <github@amund.io>
2017-10-06 14:38:25 +0200

    Separate view and increase

--- clicker/urls.py
+++ clicker/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from clicker.views import showClicks
+from clicker.views import showClicks, increaseClicks
 
 urlpatterns = [
     url(r'^$', showClicks),
+    url(r'^increase/(?P<clicker_id>[0-9]*)$', increaseClicks),
 ]
--- clicker/views.py
+++ clicker/views.py
@@ -1,20 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from clicker.models import Clicker
 
+def increaseClicks(request, clicker_id):
+    clicker_id = int(clicker_id)
+    if Clicker.objects.filter(pk=clicker_id).exists():
+        clicker = Clicker.objects.get(pk=clicker_id)
+        clicker.clicks += 1
+        clicker.save()
+
+    return HttpResponseRedirect('/')
+
 
 def showClicks(request):
-    if not Clicker.objects.exists():
-        clicker = Clicker.objects.create()
-    else:
-        clicker = Clicker.objects.first()
-
-    clicker.clicks += 1
-    clicker.save()
-
     context = {
-        'clicks': clicker.clicks
+        'clickers': Clicker.objects.all()
     }
 
     return render(request, 'clicker/clicks.html', context)
